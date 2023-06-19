de0dd4e7fd713d035fcd3954efecc769b44e4e16
kw170 <kailuwilliams@gmail.com>
2023-06-19 00:12:25 -0700

    Style book cards

--- script.js
+++ script.js
@@ -18,4 +18,16 @@ function addBookToLibrary() {
   // do stuff here
 }
 
+const popup = document.querySelector('.popup')
 const addButton = document.querySelector(".addButton")
+const submit = document.querySelector(".submit")
+
+
+// Pop up appears on add button click
+addButton.addEventListener('click',()=>{
+  popup.style.display = "flex"
+})
+
+submit.addEventListener('submit',()=>{
+  popup.style.display = "none"
+})
