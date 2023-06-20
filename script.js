a558fb06fed2364cea30952bf1fd826061084619
kw170 <kailuwilliams@gmail.com>
2023-06-20 01:16:29 -0700

    Add functions to get values from form and add new book to array

--- script.js
+++ script.js
@@ -11,16 +11,22 @@ function Book(title, author,pages,read){
 }
 
 
-let myLibrary = [];
+
 
 
 function addBookToLibrary() {
   // do stuff here
 }
 
+let myLibrary = [];
+
 const popup = document.querySelector('.popup')
 const addButton = document.querySelector(".addButton")
 const submit = document.querySelector(".submit")
+const remove = document.querySelectorAll(".remove")
+const form = document.querySelector(".form")
+
+let bookCount = 0;
 
 
 // Pop up appears on add button click
@@ -28,6 +34,33 @@ addButton.addEventListener('click',()=>{
   popup.style.display = "flex"
 })
 
-submit.addEventListener('submit',()=>{
-  popup.style.display = "none"
-})
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addBook(getBookValues());
+
+  popup.style.display = "none";
+});
+
+
+
+// Create book from form
+function getBookValues(){
+  const title = document.querySelector('#title').value;
+  console.log(title)
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const readingStatus = document.querySelector('input[name="readingStatus"]:checked').value;
+
+  return new Book(title,author,pages,readingStatus)
+
+}
+
+//add Book to array
+function addBook(newBook){
+  myLibrary.push(newBook)
+  console.log("hello")
+  console.table(myLibrary)
+}
+
+// addBook(getBookValues());
+
