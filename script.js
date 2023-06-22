0be7a6f459bbc61cb444a8e15a442a94532af113
kw170 <kailuwilliams@gmail.com>
2023-06-21 22:14:55 -0700

    Add color changing reading status feature

--- script.js
+++ script.js
@@ -53,8 +53,6 @@ exit.addEventListener('click', (e) =>{
   clearForm();
 })
 
-
-
 // Create book from form
 function getBookValues(){
   const title = document.querySelector('#title').value;
@@ -76,8 +74,19 @@ remove.forEach(removeButton =>{
   })
 })
 
-//Remove buttion functionality for new books
-
+//Change status button color
+function changeColor(readingStatusButton){
+    //Button color
+    if(readingStatusButton.textContent == "Completed"){
+      readingStatusButton.style.color = "#1e5c33";
+    }
+    else if(readingStatusButton.textContent == "Reading"){
+      readingStatusButton.style.color = "#ffa450";
+    }
+    else{
+      readingStatusButton.style.color = "#e04b3e";
+    }
+}
 
 // Add books to screen
 function addBooksToScreen() {
@@ -119,9 +128,27 @@ function addBooksToScreen() {
     buttons.appendChild(readingStatusButton)
     buttons.appendChild(remove)
 
+    remove.addEventListener('click', () =>{
+      remove.parentNode.parentNode.remove();
+    })
 
-      remove.addEventListener('click', () =>{
-        remove.parentNode.parentNode.remove();
-      })
+//Read status button
+  readingStatusButton.addEventListener('click',button =>{
+    const status = ["Completed", "Reading", "Plan to Read"]
+    if(readingStatusButton.textContent == "Completed"){
+      readingStatusButton.textContent = "Reading"
+    }
+    else if(readingStatusButton.textContent == "Reading"){
+      readingStatusButton.textContent = "Plan to Read"
+    }
+    else if(readingStatusButton.textContent == "Plan to Read"){
+      readingStatusButton.textContent = "Completed"
+    }
+    changeColor(readingStatusButton)
+  })  
+  changeColor(readingStatusButton)
 }
+
+
+
 
